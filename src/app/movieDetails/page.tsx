ed7004143e70195869cd62ba07d10f1ecbb1e5ad
Furkan <furkan5590@hotmail.com>
2023-12-28 18:20:43 +0300

    detail-page has done

--- src/app/movieDetails/page.tsx
+++ src/app/movieDetails/page.tsx
@@ -1,19 +1,76 @@
-"use client"
+"use client";
+import axios from "axios";
 import { useSearchParams } from "next/navigation";
-import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
-
-const page = () => {
-/*   const router = useRouter();
-  const searchParams = useSearchParams();
-  const id  =  searchParams.get("id") */
+const page: React.FC = () => {
+  interface MovieData {
+    adult: boolean;
+    backdrop_path: string;
+    belongs_to_collection: {
+      id: number;
+      name: string;
+      poster_path: string;
+      backdrop_path: string;
+    };
+    budget: number;
+    genres: { id: number; name: string }[];
+    overview:string;
+    homepage: string;
+    id: number;
+    imdb_id: string;
+    original_language: string;
+    title: string;
+    vote_average:number;
+    release_date:string;
+    status:string;
+    // Add other properties as needed
+  }
   const searchParams = useSearchParams();
   const id = searchParams.get("id");
-  console.log(id , " iddd  ")
+  const [movieData, setMovieData] = useState<MovieData | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        console.log("its working");
+        const response = await axios.get(
+          `https://api.themoviedb.org/3/movie/${id}?api_key=84b407c4d68abcbbcd5d97262a8f0148`
+        );
+        if (response.status !== 200) {
+          throw new Error("Failed to fetch data");
+        }
+        //  const data: MovieData = await response.json();
+
+        console.log("dataa ", response.data);
+        setMovieData(response.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, [id]);
+
   return (
+    <div className="main-area w-[75%] mx-auto flex   my-10" >
+      <div className="left-side">
+        <img
+          className="bg-cover max-h-[550px]"
+          src={`https://image.tmdb.org/t/p/w500/${movieData?.poster_path}`}
+          alt=""
+        />
+      </div>
+      <div className="right-side  ml-5 px-5 flex flex-col w-1/2 gap-4" >
+        <div className="text-white font-semibold text-2xl">{movieData?.title}</div>
+        <div className="text-white">{movieData?.overview}</div>
+        <div className="text-white">{movieData?.vote_average}</div>
+        <div className="text-white">{movieData?.release_date}</div>
+        <div className="text-white">{movieData?.status}</div>
 
-    <div>{id}</div>
-  )
-}
+      </div>
+    </div>
+  );
+};
 
-export default page
+export default page;
--- src/components/Navbar/Logo.tsx
+++ src/components/Navbar/Logo.tsx
@@ -1,7 +1,7 @@
 
 const Logo = () => {
   return (
-    <div className="text-[#800000] font-bold text-3xl cursor-pointer" >BurcuCenneti<span className="text-white text-lg">.com</span></div>
+    <div className="text-[#800000] font-bold text-3xl transition ease-in-out hover:translate-x-1 cursor-pointer hover:text-[#d55d5d] hover:scale-120 duration-500"  >BurcuCenneti<span className="text-white text-lg">.com</span></div>
   )
 }
 
